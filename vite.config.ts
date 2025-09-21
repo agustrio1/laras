0c160aa640ee0f989818049f5a088900f402861a
agustrio1 <agustrio1998@gmail.com>
2025-09-21 21:34:52 +0700

    Fix Vercel deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: ".",
   build: {
-    lib: {
-      entry: "laras.ts",
-      name: "Laras",
-      formats: ["es", "cjs", "umd"],
-      fileName: (format) => {
-        switch (format) {
-          case "es":
-            return "laras.esm.js";
-          case "cjs":
-            return "laras.cjs.js";
-          case "umd":
-            return "laras.umd.js";
-          default:
-            return "laras.js";
-        }
-      },
-    },
+    outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
-      },
-    },
+      input: {
+        main: "index.html"
+      }
+    }
   },
   server: {
     open: true,
